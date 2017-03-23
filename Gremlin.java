ca979f6532c3301e1f36232272729576e528beed
rsemeon1 <rss0024@auburn.edu>
2017-03-22 20:49:20 -0500

    Update Gremlin.java

--- Gremlin.java
+++ Gremlin.java
@@ -10,6 +10,7 @@ public class Gremlin {
 		byte[] message = packet.getData();
 		InetAddress ip;
 		
+		
 		if (address == "local")
 		 {
 			  ip = InetAddress.getLocalHost(); 
@@ -30,8 +31,12 @@ public class Gremlin {
 		if(chanceToDamage % 2 == 0){
 		
 			if ((0.0 <= prob) && (prob <= 1.0)){
-			
+		
 				prob = prob * 10;
+				
+				int percent = Math.round(prob);
+				percent = percent * 10;
+				System.out.printf("\nProbability to damage packet: %d %%\n", percent);
 				
 				Random rand2 = new Random();
 				int damage = rand2.nextInt(10);
@@ -42,16 +47,19 @@ public class Gremlin {
 					int byteCorrupt = rand3.nextInt(10);
 					
 					if(byteCorrupt < 5){
+						System.out.println("\nDamaging 1 byte in packet\n");
 						int oneByte = rand.nextInt(128);
 						message[oneByte] = message[oneByte-1];
 					}
 					else if((5 <= byteCorrupt) && (byteCorrupt < 8)){
+						System.out.println("\nDamaging 2 bytes in packet\n");
 						int firstByte = rand.nextInt(128);
 						message[firstByte] = message[firstByte-1];
 						int secondByte = rand.nextInt(128);
 						message[secondByte] = message[secondByte-1];
 					}
 					else if ((8 <= byteCorrupt) && (byteCorrupt < 10)){
+						System.out.println("\nDamaging 3 bytes in packet\n");
 						int byteOne = rand.nextInt(128);
 						int byteTwo = rand.nextInt(128);
 						int byteThree = rand.nextInt(128);
